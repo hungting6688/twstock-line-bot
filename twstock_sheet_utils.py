d45a58dd33564ab9fc74d736a15316190b802d70
hungting6688 <85077661+hungting6688@users.noreply.github.com>
2025-04-30 22:35:27 +0800

    Add files via upload

--- twstock_sheet_utils.py
+++ twstock_sheet_utils.py
@@ -1,18 +1,1 @@
-import os
-import json
-import gspread
-from google.oauth2.service_account import Credentials
-
-
-def load_sheet_data():
-    with open("twstock-bot-c384c7951791.json") as f:
-        keyfile_dict = json.load(f)
-
-    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-    creds = Credentials.from_service_account_info(keyfile_dict, scopes=scopes)
-    client = gspread.authorize(creds)
-
-    sheet = client.open_by_key(os.getenv("GOOGLE_SHEET_ID")).sheet1
-    rows = sheet.get_all_records()
-    return rows
-
+# Google Sheet 工具（讀 JSON 金鑰）
