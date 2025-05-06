acd98fe526ef2906f7d32ed8c2ca0b0a8a9b721a
hungting6688 <85077661+hungting6688@users.noreply.github.com>
2025-05-06 13:36:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # main.py
 
 from modules.run_opening import analyze_opening
+from modules.line_bot import send_line_bot_message  # ✅ 新增推播功能
 
 def main(mode):
     if mode == "opening":
         msg = analyze_opening()
-        print(msg)
+        print("[main] 分析結果如下：\n", msg)
+        send_line_bot_message(message=msg)  # ✅ 實際推播
     else:
         print("不支援的模式")
 
